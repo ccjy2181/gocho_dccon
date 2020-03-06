7ceeed07086293d5b20dce45a523a74fab619b60
gocho2 <kckwz@hanmail.net>
2020-03-06 17:17:24 +0900

    no message

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -29,11 +29,11 @@ dcConsData = [
 	
 	
 	
-	{name:"고초박수.gif",    keywords:["고초박수"],		tags:["고초콘"]},
 	{name:"고초기타.gif",    keywords:["고초기타"], 		tags:["고초콘"]},
 	{name:"고초기타2.gif",   keywords:["고초기타2"], 		tags:["고초콘"]},
 	{name:"고초기타3.gif",   keywords:["고초기타3"], 		tags:["고초콘"]},
 	{name:"고초기타4.gif",   keywords:["고초기타4"], 		tags:["고초콘"]},
+	{name:"고초박수.gif",    keywords:["고초박수"], 		tags:["고초콘"]},
 	{name:"고초노노.gif",    keywords:["고초노노"], 		tags:["고초콘"]},
 	{name:"고초노래.gif",    keywords:["고초노래"], 		tags:["고초콘"]},
 	{name:"고초눈물.gif",    keywords:["고초눈물"], 		tags:["고초콘"]},
