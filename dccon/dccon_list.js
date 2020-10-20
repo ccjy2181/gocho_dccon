b29bede5eda12941211ddf70b4ef3938b85c206b
gocho2 <kckwz@hanmail.net>
2020-10-20 18:23:13 +0900

    10.20 포멧후 리워크

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -13,6 +13,10 @@ dcConsData = [
 	{name:"고초생각.gif",   	keywords:["고초생각"],		tags:["고초콘"]},
 	{name:"고초안녕.gif",   	keywords:["고초안녕"],		tags:["고초콘"]},
 	{name:"고초어떡.gif",   	keywords:["고초어떡"],		tags:["고초콘"]},
+	
+	{name:"고초그건쫌.gif",   keywords:["고초그건쫌"], 		tags:["고초콘"]},
+	{name:"고초이건쫌.gif",   keywords:["고초이건쫌"], 		tags:["고초콘"]},
+	
 	{name:"고초덩실.gif",   	keywords:["고초덩실"],		tags:["고초콘"]},
 	{name:"고초덩실2.gif",  	keywords:["고초덩실2"],		tags:["고초콘"]},
 	{name:"고초멍.gif",   	keywords:["고초멍"],			tags:["고초콘"]},
@@ -21,11 +25,11 @@ dcConsData = [
 	{name:"고초안녕3.gif",   keywords:["고초안녕3"],		tags:["고초콘"]},
 	{name:"고초잘한다.gif",  	keywords:["고초잘한다"],		tags:["고초콘"]},
 	{name:"고초깜놀.gif",  	keywords:["고초깜놀"],		tags:["고초콘"]},
-	{name:"고초덜덜.gif",  	keywords:["고초덜덜"],		tags:["고초콘"]},
-	
-	
 	{name:"고초왼쪽.gif",    keywords:["고초왼쪽"], 		tags:["고초콘"]},
 	{name:"고초오른쪽.gif", 	 keywords:["고초오른쪽"], 		tags:["고초콘"]},
+	
+	{name:"고초덜덜.gif",  	keywords:["고초덜덜"],		tags:["고초콘"]},
+	
 	{name:"고초박수.gif", 	 keywords:["고초박수"], 		tags:["고초콘"]},
 	
 	
@@ -40,8 +44,7 @@ dcConsData = [
 	{name:"고초락커.gif",    keywords:["고초락커"], 		tags:["고초콘"]},
 	{name:"고초샷건.gif",    keywords:["고초샷건"], 		tags:["고초콘"]},
 	{name:"고초오키.gif",    keywords:["고초오키"], 		tags:["고초콘"]},
-	{name:"고초그건쫌.gif",   keywords:["고초그건쫌"], 		tags:["고초콘"]},
-	{name:"고초이건쫌.gif",   keywords:["고초이건쫌"], 		tags:["고초콘"]},
+	
 	
 		
 	
@@ -65,22 +68,11 @@ dcConsData = [
     //ppap//
    	{name:"PPAP.gif",		keywords:["PPAP"],	tags:["PPAP"]},
 	
-	
-	//군대//
-	{name:"굳건이.jpg",		keywords:["굳건이"],	tags:["군대"]},
-	
-	
-	
-	//오버워치//
-	{name:"눈물한조.png",		keywords:["눈물한조"],	tags:["오버워치"]},
-	
+		
 	//창렬//
 	{name:"창렬.gif",		keywords:["창렬"],	tags:["창렬콘"]},
 	{name:"창렬2.gif",	keywords:["창렬2"],	tags:["창렬콘"]},
-	
-	
-	
-	//해외정치인//
+		//해외정치인//
 	{name:"럼프따봉.png",		keywords:["럼프따봉"],	tags:["트럼프"]},
 	{name:"리듬스탈.gif",		keywords:["리듬스탈"],	tags:["스탈린"]},
 
@@ -88,9 +80,7 @@ dcConsData = [
 	//히오스//
 	{name:"히오스.jpg",		keywords:["히오스"],		tags:["히오스"]},
 	{name:"히오스2.gif",		keywords:["히오스2"],	tags:["히오스"]},
-	
-	
-	//WWE//
+		//WWE//
 	{name:"맥절정.gif",		keywords:["맥절정"],		tags:["wwe"]},
 	
 	
@@ -132,31 +122,13 @@ dcConsData = [
 	{name:"샌즈.png",    	keywords:["샌즈"],	tags:["샌즈"]},
 	{name:"샌즈2.gif",   	keywords:["샌즈2"],	tags:["샌즈"]},
 	
-	
-	//마크콘//
-	{name:"스켈레톤.png",   	keywords:["스켈레톤"],	tags:["마크"]},
-	{name:"스티브.png",   	keywords:["스티브"],		tags:["마크"]},
-	{name:"알렉스.png",   	keywords:["알렉스"],		tags:["마크"]},
-	{name:"좀비.png",    	keywords:["좀비"],		tags:["마크"]},
-    {name:"좀비.png",    	keywords:["좀비"],		tags:["마크"]},
-
-	
+		
 	
 
-
-
-	
-	////////////리그 오브 레전드/////////////
 	{name:"꿀벌눈물.jpg",		    keywords:["꿀벌눈물"],	tags:["롤"]},
 	{name:"꿀벌화남.jpg",		    keywords:["꿀벌화남"],	tags:["롤"]},
 	{name:"꿀벌행복.png",		    keywords:["꿀벌행복"],	tags:["롤"]},
-	{name:"카이사.png",		        keywords:["카이사"],		tags:["롤"]},
-	{name:"가렌행복.png",		    keywords:["가렌행복"],	tags:["롤"]},
-	{name:"가렌박력.gif",		    keywords:["가렌박력"],	tags:["롤"]},
-	{name:"티모.png",		   	    keywords:["티모"],		tags:["롤"]},
-	
-	
-	
+		
 	
 	////냥이콘/////////
 	{name:"냥이눈물.gif",		    keywords:["냥이눈물"],		tags:["냥이"]},
@@ -186,19 +158,6 @@ dcConsData = [
 	{name:"이풍당당.gif",		keywords:["이풍당당"],	tags:["당당"]},
 	{name:"이풍당당2.gif",		keywords:["이풍당당2"],	tags:["당당"]},
 
-	
-	//호랑이/////////
-	
-	{name:"원순1.png",	    keywords:["원순1"],	tags:["박원순"]},
-	{name:"원순2.png",	    keywords:["원순2"],	tags:["박원순"]},
-	{name:"원순3.png",	    keywords:["원순3"],	tags:["박원순"]},
-	{name:"원순4.png",	    keywords:["원순4"],	tags:["박원순"]},
-	{name:"원순5.png",	    keywords:["원순5"],	tags:["박원순"]},
-	{name:"원순6.png",	    keywords:["원순6"],	tags:["박원순"]},
-	{name:"원순7.png",	    keywords:["원순7"],	tags:["박원순"]},
-	
-	
-	
 	
 	
 	
@@ -246,21 +205,7 @@ dcConsData = [
 	{name:"페페주먹.png",    keywords:["페페주먹"],	tags:["페페"]},	
 	
 	
-	///스폰지밥콘//
-	{name:"뚱이무한.gif",     keywords:["뚱이무한"],		tags:["스폰지밥"]},	
-	{name:"뚱이사랑.gif",     keywords:["뚱이사랑"],		tags:["스폰지밥"]},	
-	{name:"뚱이휴식.gif",     keywords:["뚱이휴식"],		tags:["스폰지밥"]},	
-	{name:"스폰덜덜.gif",     keywords:["스폰덜덜"],		tags:["스폰지밥"]},	
-	{name:"스폰만세.gif",     keywords:["스폰만세"],		tags:["스폰지밥"]},	
-	{name:"스폰웃음.gif",     keywords:["스폰웃음"],		tags:["스폰지밥"]},	
-	{name:"스폰헉헉.gif",     keywords:["스폰헉헉"],		tags:["스폰지밥"]},	
-	{name:"스폰현피.gif",     keywords:["스폰현피"],		tags:["스폰지밥"]},	
-	{name:"스폰활활.gif",     keywords:["스폰활할"],		tags:["스폰지밥"]},	
-	{name:"스폰사랑.gif",     keywords:["스폰사랑"],		tags:["스폰지밥"]},	
-	{name:"징징분노.gif",     keywords:["징징분노"],		tags:["스폰지밥"]},	
-	{name:"징징이.gif",       keywords:["징징이"],		tags:["스폰지밥"]},	
-	{name:"징징한숨.gif",     keywords:["징징한숨"],		tags:["스폰지밥"]},	
-	
+
     //타짜콘//
 	{name:"타짜갈때.gif",      keywords:["타짜갈때"],			tags:["타짜"]},	
 	{name:"타짜따라란.gif",    keywords:["타짜따라란"],		tags:["타짜"]},	
@@ -268,13 +213,7 @@ dcConsData = [
 	{name:"타짜왜이러.gif",    keywords:["타짜왜이러"],		tags:["타짜"]},	
 	{name:"타짜충격.gif",      keywords:["타짜충격"],			tags:["타짜"]},	
 	
-	///참피//
-	{name:"참피런.gif",      keywords:["참피런"],			tags:["참피"]},	
-	{name:"참피런2.gif",     keywords:["참피런2"],		tags:["참피"]},	
-	{name:"참피엉덩.gif",    keywords:["참피엉덩"],		tags:["참피"]},	
-	{name:"참피그만.gif",    keywords:["참피그만"],		tags:["참피"]},	
-	
-	
+
 	//리사수//
 	{name:"리사수1.gif",    keywords:["리사수1"],		tags:["리사수"]},	
 	{name:"리사수2.gif",    keywords:["리사수2"],		tags:["리사수"]},	
@@ -310,7 +249,6 @@ dcConsData = [
     {name:"겉바속촉.gif", 	    keywords:["겉바속촉"],	tags:["와우", "헬스크림"]},
 	{name:"흠.png", 	  	    keywords:["흠"],   		tags:["페페"]},
 	{name:"버그.gif", 	  		keywords:["버그"],   	tags:["버그"]},
-	{name:"던질까말까.gif", 	  	keywords:["던질까말까"], 	tags:["던질까말까"]},  //
 	{name:"민트초코.jpg", 	  	keywords:["민트초코"], 	tags:["민트초코"]},
     
 	
@@ -323,105 +261,13 @@ dcConsData = [
 	{name:"7점.png",			keywords:["7점"],		tags:["점수"]},
 	{name:"8점.png",			keywords:["8점"],		tags:["점수"]},
 	{name:"9점.png",			keywords:["9점"],		tags:["점수"]},
-	{name:"10점.png",			keywords:["10점"],		tags:["점수"]},
+	{name:"10점.png",			keywords:["10점"],		tags:["점수"]}
 	
 	
 	
 	
 	
 	
-	////////////롤코타/////////////
-	{name:"멀미.gif",		    keywords:["멀미"],		tags:["롤코타"]},
-	{name:"멀미2.gif",		keywords:["멀미2"],		tags:["롤코타"]},
-	{name:"멀미3.gif",		keywords:["멀미3"],		tags:["롤코타"]},
-	{name:"피곤.gif",		    keywords:["피곤"],		tags:["롤코타"]},
-	{name:"행복.gif",		    keywords:["행복"],		tags:["롤코타"]},
-	{name:"행복2.gif",	    keywords:["행복2"],		tags:["롤코타"]},
-	{name:"화남.gif",	        keywords:["화남"],		tags:["롤코타"]},
-	{name:"ATM.png",		keywords:["atm"],		tags:["롤코타"]},
-	{name:"화장실.png",		keywords:["화장실"],		tags:["롤코타"]},
-	{name:"회전목마.png",		keywords:["회전목마"],		tags:["롤코타"]},
-	{name:"불행.gif",	     	keywords:["불행"],		tags:["롤코타"]},
-	{name:"불행2.gif",		keywords:["불행2"],		tags:["롤코타"]},
-    {name:"안내소.png",		keywords:["안내소"],		tags:["롤코타"]},	
-	{name:"하트라인.gif",		keywords:["하트라인"],	 	tags:["롤코타"]},
-    {name:"2층전망탑.png",		keywords:["2층전망탑"],	tags:["롤코타"]},	
-	{name:"360도바이킹.png",	keywords:["360도바이킹"],	tags:["롤코타"]},	
-	{name:"고카트.png",		keywords:["고카트"],		tags:["롤코타"]},	
-	{name:"골프.png",		    keywords:["골프"],		tags:["롤코타"]},	
-	{name:"관람차.png",		keywords:["관람차"],		tags:["롤코타"]},	
-	{name:"귀신의집.png",		keywords:["귀신의집"],		tags:["롤코타"]},	
-	{name:"기가.png",		    keywords:["기가"],		tags:["롤코타"]},	
-	{name:"나선.png",		    keywords:["나선"],		tags:["롤코타"]},	
-	{name:"런치드프리폴.png",	keywords:["런치드프리폴"],	tags:["롤코타"]},	
-	{name:"레이다운.png",		keywords:["레이다운"],		tags:["롤코타"]},	
-	{name:"루핑.png",			keywords:["루핑"],		tags:["롤코타"]},	
-	{name:"리버래프트.png",		keywords:["리버래프트"],	tags:["롤코타"]},	
-	{name:"리버서.png",		keywords:["리버서"],		tags:["롤코타"]},	
-	{name:"리버래피드.png",		keywords:["리버래피드"],	tags:["롤코타"]},	
-	{name:"림발진.png",		keywords:["림발진"],		tags:["롤코타"]},	
-	{name:"마인.png",			keywords:["마인"],		tags:["롤코타"]},	
-	{name:"멀티디멘션.png",		keywords:["멀티디멘션"],	tags:["롤코타"]},	
-	{name:"메이즈.png",		keywords:["메이즈"],		tags:["롤코타"]},	
-	{name:"모노레일.png",		keywords:["모노레일"],		tags:["롤코타"]},	
-	{name:"모션시뮬레이터.png",	keywords:["모션시뮬레이터"],	tags:["롤코타"]},	
-	{name:"모형기차.png",		keywords:["모형기차"],		tags:["롤코타"]},	
-	{name:"미끄럼틀.png",		keywords:["미끄럼틀"],		tags:["롤코타"]},	
-	{name:"미니.png",			keywords:["미니"],		tags:["롤코타"]},	
-	{name:"미니서스펜디드.png",	keywords:["미니서스펜디드"],	tags:["롤코타"]},	
-	{name:"미니헬리콥터.png",	keywords:["미니헬리콥터"],	tags:["롤코타"]},	
-	{name:"바이킹.png",		keywords:["바이킹"],		tags:["롤코타"]},	
-	{name:"백조보트.png",		keywords:["백조보트"],		tags:["롤코타"]},	
-	{name:"버지니아.png",		keywords:["버지니아"],		tags:["롤코타"]},	
-	{name:"범퍼카.png",		keywords:["범퍼카"],		tags:["롤코타"]},	
-	{name:"보트.png",			keywords:["보트"],		tags:["롤코타"]},	
-	{name:"봅슬레이.png",		keywords:["봅슬레이"],		tags:["롤코타"]},	
-	{name:"비행접시.png",		keywords:["비행접시"],		tags:["롤코타"]},	
-	{name:"삐뚤어진집.png",		keywords:["삐뚤어진집"],	tags:["롤코타"]},	
-	{name:"사이드프릭션.png",	keywords:["사이드프릭션"],	tags:["롤코타"]},	
-	{name:"서스펜디드.png",		keywords:["서스펜디드"],	tags:["롤코타"]},	
-	{name:"서스펜디드모노레일.png",keywords:["서스펜디드모노레일"],tags:["롤코타"]},	
-	{name:"서커스.png",		keywords:["서커스"],		tags:["롤코타"]},	
-	{name:"솜사탕가게.png",		keywords:["솜사탕가게"],	tags:["롤코타"]},	
-	{name:"수직낙하.gif",		keywords:["수직낙하"],		tags:["롤코타"]},	
-	{name:"수직낙하2.gif",		keywords:["수직낙하2"],	tags:["롤코타"]},	
-	{name:"스탠드업.png",		keywords:["스탠드업"],		tags:["롤코타"]},	
-	{name:"스티플체이스.png",	keywords:["스티플체이스"],	tags:["롤코타"]},	
-	{name:"스파이럴.png",		keywords:["스파이럴"],		tags:["롤코타"]},	
-	{name:"스플래시보트.png",	keywords:["스플래시보트"],	tags:["롤코타"]},	
-	{name:"양탄자.png",		keywords:["양탄자"],		tags:["롤코타"]},	
-	{name:"에어파워드버티컬.png",	keywords:["에어파워드버티컬"],tags:["롤코타"]},	
-	{name:"엔터프라이즈.png",	keywords:["엔터프라이즈"],	tags:["롤코타"]},	
-	{name:"엘리베이터.png",		keywords:["엘리베이터"],	tags:["롤코타"]},	
-	{name:"역방향자유낙하.png",	keywords:["역방향자유낙하"],	tags:["롤코타"]},	
-	{name:"영화관.png",		keywords:["영화관"],		tags:["롤코타"]},	
-	{name:"와일드마우스.png",	keywords:["와일드마우스"],	tags:["롤코타"]},	
-	{name:"우든.png",			keywords:["우든"],		tags:["롤코타"]},	
-	{name:"우든와일드.png",		keywords:["우든와일드"],	tags:["롤코타"]},	
-	{name:"우든와일드광산.png",	keywords:["우든와일드광산"],	tags:["롤코타"]},	
-	{name:"워터슬라이드.png",	keywords:["워터슬라이드"],	tags:["롤코타"]},	
-	{name:"워터코스터.png",		keywords:["워터코스터"],	tags:["롤코타"]},	
-	{name:"유령기차.png",		keywords:["유령기차"],		tags:["롤코타"]},	
-	{name:"인버티드.png",		keywords:["인버티드"],		tags:["롤코타"]},	
-	{name:"자이로드롭.png",		keywords:["자이로드롭"],	tags:["롤코타"]},	
-	{name:"자전거.png",		keywords:["자전거"],		tags:["롤코타"]},	
-	{name:"전망탑.png",		keywords:["전망탑"],		tags:["롤코타"]},	
-	{name:"전차.png",			keywords:["전차"],		tags:["롤코타"]},	
-	{name:"주니어.png",		keywords:["주니어"],		tags:["롤코타"]},	
-	{name:"체어리프트.png",		keywords:["체어리프트"],	tags:["롤코타"]},	
-	{name:"촉수가게.png",		keywords:["촉수가게"],		tags:["롤코타"]},	
-	{name:"치킨가게.png",		keywords:["치킨가게"],		tags:["롤코타"]},	
-	{name:"카라이드.png",		keywords:["카라이드"],		tags:["롤코타"]},	
-	{name:"컴팩트인버티드.png",	keywords:["컴팩트인버티드"],	tags:["롤코타"]},	
-	{name:"탄광.png",			keywords:["탄광"],		tags:["롤코타"]},	
-	{name:"탑스핀.png",		keywords:["탑스핀"],		tags:["롤코타"]},	
-	{name:"트위스터.png",		keywords:["트위스터"],		tags:["롤코타"]},	
-	{name:"트위스트.png",		keywords:["트위스트"],		tags:["롤코타"]},	
-	{name:"풍선가게.png",		keywords:["풍선가게"],		tags:["롤코타"]},	
-	{name:"플라잉.png",		keywords:["플라잉"],		tags:["롤코타"]},	
-	{name:"플라잉턴.png",		keywords:["플라잉턴"],		tags:["롤코타"]},	
-	{name:"플로어리스.png",		keywords:["플로어리스"],	tags:["롤코타"]},	
-	{name:"후룸라이드.png",		keywords:["후룸라이드"],	tags:["롤코타"]}	
 	
 	
 	
