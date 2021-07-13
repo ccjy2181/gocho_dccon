8e3fef813512aa3c9cb5441e5e6777cd74246ad8
gocho2 <kckwz@hanmail.net>
2021-07-13 15:14:51 +0900

    no message

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -152,8 +152,8 @@ dcConsData = [
 	
 	///스타크래프트///
 	{name:"콩탈리스크.gif",		keywords:["콩탈리스크"],	tags:["스타크래프트"]},
-	{name:"장판파.gif",		keywords:["장판파"],	tags:["스타크래프트"]},
-	{name:"테란일꾼.gif",		keywords:["테란일꾼],	tags:["스타크래프트"]},
+	{name:"장판파.jpg",		keywords:["장판파"],	tags:["스타크래프트"]},
+	{name:"테란일꾼.gif",		keywords:["테란일꾼"],	tags:["스타크래프트"]},
 	
 	
 	
