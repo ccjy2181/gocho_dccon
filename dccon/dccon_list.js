47a6bdbdd1ad7176350d9c377a0b9b859ee8670e
gocho2 <kckwz@hanmail.net>
2021-07-13 19:07:16 +0900

    no message

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -168,7 +168,6 @@ dcConsData = [
 	{name:"이제동맥경화.gif",		keywords:["이제동맥경화"], tags:["스타크래프트"]},
 	{name:"이제동뮤탈.gif",		keywords:["이제동뮤탈"],	tags:["스타크래프트"]},
 	{name:"임요환마린.gif",		keywords:["임요환마린"],	tags:["스타크래프트"]},
-	{name:"장판파.gif",			keywords:["장판파"],		tags:["스타크래프트"]},
 	{name:"천지스톰.gif",			keywords:["천지스톰"],	tags:["스타크래프트"]},
 	
 	
